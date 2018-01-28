d68ed744094b9955eb7ec37e82140d78602372b3
AresEntretainement <35566353+AresEntretainement@users.noreply.github.com>
2018-01-28 19:09:41 +0100

    Test moteur

--- intelligencia/classRobot/composantMotorDC.py
+++ intelligencia/classRobot/composantMotorDC.py
@@ -19,39 +19,26 @@ class MotorDC:
 
 ## Controle d'un moteur DC par le Raspberry Pi
 
+        GPIO.setmode(GPIO.BCM)              # GPIO Numbering
+
+        OutLine = 16      ## premiere sortie du premier moteur, pin 16
+        AlphaRaport = 18      ## deuxieme sortie de premier moteur, pin 18
+        Truc = 22      ## enable du premier moteur, pin 22
+
+        GPIO.setup(self.OutLine,GPIO.OUT)  # All pins as Outputs
+        GPIO.setup(self.AlphaRaport,GPIO.OUT)
+        GPIO.setup(self.Truc,GPIO.OUT)
+
+    async def moteur(self, pause): 
 
 
-GPIO.setmode(GPIO.BOARD)   ##apparemment y a une différence de syntaxe entre BCM et BOARD, mais vu que j'ai pas compris, j'propose qu'on laisse et qu'on fasse genre "ouais c'est pour exploiter au mieux Python"
 
-Moteur1A = 16      ## premiere sortie du premier moteur, pin 16
-Moteur1B = 18      ## deuxieme sortie de premier moteur, pin 18
-Moteur1E = 22      ## enable du premier moteur, pin 22
+            GPIO.output(self.OutLine,GPIO.HIGH)
+            GPIO.output(self.AlphaRaport,GPIO.LOW)
+            GPIO.output(self.Truc,GPIO.HIGH)
+ 
+            sleep(pause)
+            # Stop
+            GPIO.output(self.Truc,GPIO.LOW)
 
-GPIO.setup(Moteur1A,GPIO.OUT)  ## ces trois pins du Raspberry Pi sont des sorties
-GPIO.setup(Moteur1B,GPIO.OUT)
-GPIO.setup(Moteur1E,GPIO.OUT)
-
-async def moteur(self):
-
-    pwm = GPIO.PWM(Moteur1E,50)   ## pwm de la pin 22 a une frequence de 50 Hz
-    pwm.start(20)   ## on commence avec un rapport cyclique de 20% => accélération vers 50% puis 100% pour pas que le moteur soit en position "appelez les hendeks"
-
-    GPIO.output(Moteur1A,GPIO.HIGH)
-    GPIO.output(Moteur1B,GPIO.LOW)
-    GPIO.output(Moteur1E,GPIO.HIGH)
-
-    sleep(0.2)
-
-    pwm.ChangeDutyCycle(50)  ## modification du rapport cyclique a 50%
-
-    sleep(0.2)
-
-    pwm.ChangeDutyCycle(100) ## modification du rapport cyclique à 100%
-
-    sleep(5) ## temporisation avant de couper le moteur et arrêter la sparbat, à redéfinir
-
-    GPIO.output(Moteur1E,GPIO.LOW)
-
-    pwm.stop()    ## interruption du pwm
-
-    GPIO.cleanup()
+            GPIO.cleanup()
